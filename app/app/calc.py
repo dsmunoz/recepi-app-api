1a8c5e779ccdef7a9136dd1bb0ab66e4f1d9fc14
dmunoz <diego.munoz@beon.studio>
2022-10-27 17:02:09 -0700

    adding tests and pgsql

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add X and Y and resturn result. """
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
